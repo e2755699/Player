fe145c0637a87314a845249a184d2d05ac4af85b
Dustin <e2755699@gmail.com>
2020-01-21 01:45:35 +0800

    Update PlaceBet.cs
    add username and pwd

--- Player/PlaceBet.cs
+++ Player/PlaceBet.cs
@@ -8,7 +8,7 @@ namespace Player
     {
         static void Main(string[] args)
         {
-            var factory = new ConnectionFactory() { HostName = "10.0.15.40" };
+      var factory = new ConnectionFactory() { HostName = "10.0.15.40" , UserName = "rabbit", Password = "QCctDqnyu2ig" };
             using (var connection = factory.CreateConnection())
             {
                 using (var channel = connection.CreateModel())
